5b91ea645f6dcb868c00a75ec2eaaa68a1645284
Larissa <lbarra@thoughtworks.com>
2018-03-27 17:25:40 -0300

    [logic] lbarra: adds checklistItem class

---
+++ Checklists/ChecklistItem.swift
@@ -0,0 +1,19 @@
+//
+//  ChecklistItem.swift
+//  Checklists
+//
+//  Created by Larissa Barra Conde on 27/03/18.
+//  Copyright © 2018 CapDev ThoughtWorks. All rights reserved.
+//
+
+import Foundation
+
+class ChecklistItem {
+    var text = ""
+    var checked = false
+    
+    init(text : String, checked : Bool) {
+        self.text = text
+        self.checked = checked
+    }
+}
