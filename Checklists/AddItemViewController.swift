deb6d7687d7a6d40054e90c2d86b108c10356034
Larissa <lbarra@thoughtworks.com>
2018-03-28 15:29:01 -0300

    [logic] lbarra: adds new item controller and binds it to view on storyboard

---
+++ Checklists/AddItemViewController.swift
@@ -0,0 +1,16 @@
+//
+//  AddItemViewController.swift
+//  Checklists
+//
+//  Created by Larissa Barra Conde on 28/03/18.
+//  Copyright © 2018 CapDev ThoughtWorks. All rights reserved.
+//
+
+import UIKit
+
+class AddItemViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
