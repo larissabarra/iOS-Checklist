9380adfcc313a43dc32c498e96c3d61f98f8ee5e
Larissa <lbarra@thoughtworks.com>
2018-03-28 15:33:56 -0300

    [logic] lbarra: adds dismiss action to nav bar buttons on add item screen

--- Checklists/AddItemViewController.swift
+++ Checklists/AddItemViewController.swift
@@ -10,7 +10,17 @@ import UIKit
 
 class AddItemViewController: UITableViewController {
 
+    @IBAction func cancel(_ sender: UIBarButtonItem) {
+        navigationController?.popViewController(animated: true)
+    }
+    
+    @IBAction func done(_ sender: UIBarButtonItem) {
+        navigationController?.popViewController(animated: true)
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        navigationItem.largeTitleDisplayMode = .never
     }
 }
