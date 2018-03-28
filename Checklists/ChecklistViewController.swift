b74143960b023ecb982b50fc1b8c0cd0c658d087
Larissa <lbarra@thoughtworks.com>
2018-03-28 11:34:02 -0300

    [logic] lbarra: adds functionality to new item button

--- Checklists/ChecklistViewController.swift
+++ Checklists/ChecklistViewController.swift
@@ -13,12 +13,23 @@ class ChecklistViewController: UITableViewController {
     let dataProvider = ChecklistDataProvider()
     var items: [ChecklistItem] = []
     
+    @IBAction func addNewItem(_ sender: UIBarButtonItem) {
+        let newRowIndex = items.count
+        
+        dataProvider.addItem(text: "I am a new row")
+        loadItems()
+        
+        let indexPath = IndexPath(row: newRowIndex, section: 0)
+        let indexPaths = [indexPath]
+        tableView.insertRows(at: indexPaths, with: .automatic)
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         navigationController?.navigationBar.prefersLargeTitles = true
 
-        items = dataProvider.getItems()
+        loadItems()
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
@@ -57,6 +68,10 @@ class ChecklistViewController: UITableViewController {
     func configureText(for cell: UITableViewCell, with item: ChecklistItem) {
         let label = cell.viewWithTag(1000) as! UILabel
         label.text = item.text
+    }
+    
+    func loadItems() {
+        items = dataProvider.getItems()
     }
 }
 
