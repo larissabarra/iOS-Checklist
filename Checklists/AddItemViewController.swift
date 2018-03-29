b28d413a8f6352242186789eda340f4a44eca7f3
Larissa <lbarra@thoughtworks.com>
2018-03-29 10:42:27 -0300

    [logic] lbarra: disables done button for empty text field

--- Checklists/AddItemViewController.swift
+++ Checklists/AddItemViewController.swift
@@ -8,8 +8,9 @@
 
 import UIKit
 
-class AddItemViewController: UITableViewController {
+class AddItemViewController: UITableViewController, UITextFieldDelegate {
     
+    @IBOutlet weak var doneBarButton: UIBarButtonItem!
     @IBOutlet weak var textField: UITextField!
     
     @IBAction func cancel(_ sender: UIBarButtonItem) {
@@ -34,5 +35,14 @@ class AddItemViewController: UITableViewController {
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
         textField.becomeFirstResponder()
+    }
+    
+    /* text field delegate */
+    func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
+        let oldText = textField.text!
+        let stringRange = Range(range, in:oldText)!
+        let newText = oldText.replacingCharacters(in: stringRange, with: string)
+        doneBarButton.isEnabled = !newText.isEmpty
+        return true
     }
 }
