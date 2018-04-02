c8a4ae0e95a1205d48f3c78c04b3319000ef2414
Larissa <lbarra@thoughtworks.com>
2018-04-02 17:51:54 -0300

    [ui] lbarra: changes colours for app buttons

--- Checklists/Controllers/ChecklistViewController.swift
+++ Checklists/Controllers/ChecklistViewController.swift
@@ -97,10 +97,12 @@ class ChecklistViewController: UITableViewController, ItemDetailViewControllerDe
         let label = cell.viewWithTag(1001) as! UILabel
         
         label.text = item.checked ? "√" : ""
+        label.textColor = view.tintColor
     }
     
     func configureText(for cell: UITableViewCell, with item: ChecklistItem) {
         let label = cell.viewWithTag(1000) as! UILabel
+        
         label.text = item.text
     }
     
