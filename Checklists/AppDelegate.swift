e0ce635e003cf8c9b01a53e06ba4ca6c4fb4d0d1
Larissa <lbarra@thoughtworks.com>
2018-04-02 18:32:41 -0300

    [logic] lbarra: adds initial code to send local notifications

--- Checklists/AppDelegate.swift
+++ Checklists/AppDelegate.swift
@@ -7,9 +7,10 @@
 //
 
 import UIKit
+import UserNotifications
 
 @UIApplicationMain
-class AppDelegate: UIResponder, UIApplicationDelegate {
+class AppDelegate: UIResponder, UIApplicationDelegate, UNUserNotificationCenterDelegate {
 
     var window: UIWindow?
     let dataProvider = ChecklistDataProvider()
@@ -23,17 +24,30 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         let controller = navigationController.viewControllers[0] as! AllListsTableViewController
         controller.dataProvider = dataProvider
         
+        let center = UNUserNotificationCenter.current()
+        center.requestAuthorization(options: [.alert, .sound]) { granted, error in
+            if granted {
+                print("We have permission")
+                center.delegate = self
+            } else {
+                print("Permission denied")
+            }
+        }
+        
         return true
     }
-
+    
     func applicationDidEnterBackground(_ application: UIApplication) {
         saveData()
     }
-
+    
     func applicationWillTerminate(_ application: UIApplication) {
         saveData()
     }
-
-
+    
+    // MARK:- User Notification Delegates
+    func userNotificationCenter(_ center: UNUserNotificationCenter, willPresent notification: UNNotification, withCompletionHandler completionHandler: @escaping (UNNotificationPresentationOptions) -> Void) {
+        print("Received local notification \(notification)")
+    }
 }
 
